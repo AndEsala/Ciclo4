db483b535398be3cabcb7df4b52b4a823159ff9a
AEsala <aesala@uninorte.edu.co>
2021-11-24 08:06:51 -0500

    Contenedor de los diferentes tipos de notificaciones

---
+++ lib/ui/pages/content/states/widgets/type_state.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:red_egresados/ui/pages/content/states/widgets/state_card.dart';
+
+class ContentStates extends StatelessWidget {
+  final String title;
+  List<Widget> states;
+  ContentStates({required this.title, required this.states});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TextButton(onPressed: () {}, child: Text(title)),
+        Column(
+          children: states,
+        )
+      ],
+    );
+  }
+}
