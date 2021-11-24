d488a006cd3bab881534759cd0a77e3b433619f8
AEsala <aesala@uninorte.edu.co>
2021-11-24 08:00:19 -0500

    Notificaciones de prueba

--- lib/ui/pages/content/states/states_screen.dart
+++ lib/ui/pages/content/states/states_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:red_egresados/ui/pages/content/states/widgets/type_state.dart';
 import 'widgets/state_card.dart';
 
 class StatesScreen extends StatefulWidget {
@@ -10,18 +11,59 @@ class StatesScreen extends StatefulWidget {
 }
 
 class _State extends State<StatesScreen> {
-  final items = List<String>.generate(20, (i) => "Item $i");
+  final Icon icono = Icon(Icons.notifications);
+
+  final stateCardChat = StateCard(
+    title: 'Andrés',
+    content: 'Lorem ipsum dolor sit amet.',
+    picUrl: 'https://uifaces.co/our-content/donated/gPZwCbdS.jpg',
+    icon: const Icon(Icons.chat),
+    onChat: () => {},
+  );
+
+  final stateCardNotify = StateCard(
+    title: 'Andrés',
+    content: 'Lorem ipsum dolor sit amet.',
+    picUrl: 'https://uifaces.co/our-content/donated/gPZwCbdS.jpg',
+    icon: const Icon(Icons.notifications),
+    onChat: () => {},
+  );
+
+  final stateCardAdop = StateCard(
+    title: 'Andrés',
+    content: 'Lorem ipsum dolor sit amet.',
+    picUrl: 'https://uifaces.co/our-content/donated/gPZwCbdS.jpg',
+    icon: const Icon(Icons.pets),
+    onChat: () => {},
+  );
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: items.length,
+      itemCount: 1,
       itemBuilder: (context, index) {
-        return StateCard(
-          title: 'Iony Houst',
-          content: 'Lorem ipsum dolor sit amet.',
-          picUrl: 'https://uifaces.co/our-content/donated/gPZwCbdS.jpg',
-          onChat: () => {},
+        List<StateCard> itemsChats = [
+          stateCardChat,
+          stateCardChat,
+          stateCardChat
+        ];
+        List<StateCard> itemsNotify = [
+          stateCardNotify,
+          stateCardNotify,
+          stateCardNotify
+        ];
+        List<StateCard> itemsAdop = [
+          stateCardAdop,
+          stateCardAdop,
+          stateCardAdop
+        ];
+
+        return Column(
+          children: [
+            ContentStates(title: "Chats", states: itemsChats),
+            ContentStates(title: "Notificaciones", states: itemsNotify),
+            ContentStates(title: "Adopción", states: itemsAdop)
+          ],
         );
       },
     );
