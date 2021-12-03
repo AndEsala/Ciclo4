ded9f895f79ce752705089c9535319c8ab69942f
jumime <granadosmj@uninorte.edu.co>
2021-12-03 16:38:34 -0500

    folder test

---
+++ lib/test/widget_test.dart
@@ -0,0 +1,41 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:red_egresados/ui/app.dart';
+import 'package:red_egresados/ui/pages/authentication/login/login_screen.dart';
+import 'package:red_egresados/main.dart';
+
+void main(){
+  group('Encontrar widgets', () {
+
+    testWidgets('Buscar widgets',(WidgetTester tester) async{
+      await tester.pumpWidget(App());
+      final widget1 = find.byType(Scaffold);
+      expect(widget1, findsWidgets);
+
+      final widget2 = find.byType(TextButton);
+      expect(widget2, findsOneWidget);
+
+      final widget3 = find.byType(TextFormField);
+      expect(widget3, findsWidgets);
+
+    });
+
+
+  });
+
+  group('Interaccion con widgets', () {
+    testWidgets('Check we can type in the textfield',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(App());
+      var password = 'Hacer2345';
+      expect(find.text(password), findsNothing);
+
+      await tester.enterText(find.byType(TextFormField), password);
+      await tester.pump();
+
+      // Expect to find the item on screen.
+      expect(find.text(password), findsOneWidget);
+    });
+  });
+}    
