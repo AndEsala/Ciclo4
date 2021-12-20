1ae3ac166c11a4e2ee527295f77ceeb66f07499d
DFabian02 <tdidier@uninorte.edu.co>
2021-12-20 18:15:36 -0500

    delete files

--- lib/data/services/geolocation.dart
+++
@@ -1,9 +0,0 @@
-import 'package:geolocator/geolocator.dart';
-import 'package:red_peetoze/domain/services/location.dart';
-
-class GpsService implements LocationInterface {
-  @override
-  Future<Position> getCurrentLocation() async {
-    return await Geolocator.getCurrentPosition();
-  }
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,17 @@
-import 'dart:developer';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:red_peetoze/data/services/location.dart';
 
 import 'package:red_peetoze/domain/controller/controlchat.dart';
-import 'package:red_peetoze/domain/controller/controllerauth.dart';
+
 import 'package:red_peetoze/data/controllerrealtime.dart';
 import 'package:red_peetoze/domain/controller/firestore.dart';
 import 'package:red_peetoze/domain/controller/locations.dart';
-import 'package:red_peetoze/domain/models/location_model.dart';
-import 'package:red_peetoze/domain/use_cases/controllers/location_management.dart';
-import 'package:red_peetoze/domain/use_cases/controllers/notification.dart';
+
 import 'package:red_peetoze/domain/use_cases/controllers/permissions.dart';
-import 'package:red_peetoze/domain/use_cases/controllers/ui.dart';
+
 import 'package:red_peetoze/ui/app.dart';
 import 'package:get/get.dart';
-import 'package:red_peetoze/ui/pages/content/location/widgets/vista_location.dart';
+
 import 'package:workmanager/workmanager.dart';
 
 void main() async {
@@ -39,21 +34,9 @@ void main() async {
 }
 
 void updatePositionInBackground() async {
-  final manager = LocationManager();
-  final service = LocationService();
   Workmanager().executeTask((task, inputData) async {
-    final position = await manager.getCurrentLocation();
-    final details = await manager.retrieveUserDetails();
-    var location = MyLocation(
-        name: details['name']!,
-        id: details['uid']!,
-        lat: position.latitude,
-        long: position.longitude);
-    await service.fecthData(
-      map: location.toJson,
-    );
-    log("updated location background"); //simpleTask will be emitted here.
-    print("updated location background"); //simpleTask will be emitted here.
+    Controllerlocations controlubicacion = Get.find();
+    controlubicacion.obtenerubicacion();
     return Future.value(true);
   });
 }
